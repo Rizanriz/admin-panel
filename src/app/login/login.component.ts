3225adf3d1d6ffb94c35759afb6da6eddc063ceb
Rizanriz <rizanr7777@gmail.com>
2024-08-27 16:59:29 +0530

    ongoing commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  overview:string = ""
+
+  email:string = ""
+  password:string = ""
+
+  constructor(private router:Router,data:DataService){}
+
+  login(){
+    if (this.email && this.password) {
+      this.router.navigateByUrl("Home")
+    }else{
+      alert( `please enter the details`)
+    }
+  }
+
 }
